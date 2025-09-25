69951da5e8c5443a4d24cef6bec410730cb9316f
“suporte02” <email@email.com>
2025-09-25 17:12:32 -0300

    funcionando em um grupo

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Objeto 
+o projeto 
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 
-def api(TOKEN, BASE_URL):
+def api(TOKEN, BASE_URL,timeout):
     r = requests.get(BASE_URL + "getMe")
 
     if r.status_code != 200:
@@ -14,38 +14,53 @@ def api(TOKEN, BASE_URL):
     print("Bot online, OK")
     offset = None
     while True:
-        polingUpdate = getUpdates(TOKEN,BASE_URL, offset=offset)
-
-        if polingUpdate is not None:
-            
+        polingUpdate = getUpdates(BASE_URL, offset=offset,timeout=timeout)
+        print(polingUpdate)
+        if (polingUpdate is not None) :
+            print("Updater não esta vazio")
             offset = int(polingUpdate["update_id"]) + 1
-
-            recivedMessage(BASE_URL,polingUpdate["message"])
+            #My_chat_member serve para outra coisa no chat 
+            if "my_chat_member" not in polingUpdate and "new_chat_member" not in polingUpdate["message"]: 
+                #Aq contemos dados como id,first_name,type
+                messageObj =  polingUpdate["message"]
+                chatObj = {
+                    "id": messageObj["chat"]["id"],
+                    "title": messageObj["chat"]["title"],
+                }
+                
+                #Aq temos o texto enviado
+                textData = {
+                    "text":messageObj["text"], 
+                    "date":messageObj["date"],
+                    "first_name":messageObj["from"]["first_name"]
+                }
+                
+               
+                
+                confirmMessage = commandsController(textData=textData);
+                
+                sendMessage(BASE_URL,confirmMessage,chat_id=chatObj["id"])
         
 
 
 #message sera um objeto complexo contendo todos os dados da mensagem
-def recivedMessage(BASE_URL,message = None):  # noqa: F811
-    if message is None:
+def sendMessage(BASE_URL,text,chat_id = None):  # noqa: F811
+    if chat_id is None or text is None:
+        print(f"Valores chat_id:{chat_id} ou text:{text} None mensagem não enviada")
         return None
-    
-    chat_id = message["chat"]["id"]
-    user_first_name = message["chat"]["first_name"]
 
-
-    params = {"chat_id": chat_id,"text":f"hello {user_first_name}"}
+    print(f"Enviado messagem: {text} para o chatId : {chat_id}")
+    params = {"chat_id": chat_id,"text":text}
 
     requests.post(BASE_URL+"sendMessage",params=params)
-    
 
 
-def getUpdates(TOKEN, BASE_URL, offset=None):
+def getUpdates(BASE_URL, offset=None,timeout = 5):
     # OffSet Definindo apartir de qual mensagem pegar sendo a primeira a mais importante
-
-    params = {"timeout": 5, "offset": offset}
+    params = {"timeout": timeout, "offset": offset}
     r = requests.get(BASE_URL + "getUpdates", params=params)
 
-    print(r.json())
+    # print(r.json())
 
     if "result" not in r.json():
         return None
@@ -57,8 +72,63 @@ def getUpdates(TOKEN, BASE_URL, offset=None):
     return r.json()["result"][0]
 
 
+def commandsController(textData):
+    print("Cotroller commands iniciado")
+    
+    #Processa a mensagem para retirar o comando e armazenar a mensagem
+    breakText = (textData["text"].strip()).split(" ")
+    command = breakText[0]
+    text = " ".join(breakText[1:])
+        
+    #Essa função de controle, aonde os comandos vão ser tratados e executados
+    commands = {
+        "/start" : lambda : welcomeMessage(textData["first_name"]), # type: ignore
+        "/save" :lambda : saveInDataBase(text),
+        "/list" : listAllShoppingWithOutDate(),
+        "/ldat" : listAllShoppingBetweenDates(),
+        "/expe":getTotalExpensesgWithOutDate(),
+        "/edat":getTotalExpensesBetweenDate(),
+        "/cat" : createNewCategoryInDataBase()
+    }
+    
+  
+    if command not in commands:
+        print("comando não encontrado na lista de comandos")
+        return None
+    
+    print("Cotroller retornando valor")
+    funcInstance = commands.get(f"{command}")
+    mensageReturn = funcInstance()
+    print(f"Mensagem de retorno: {mensageReturn}")
+    return mensageReturn
+
+    
+def welcomeMessage(userName = None):
+    print("função welcomeMessage iniciada!")
+    return f"Grettings Welcome! {userName}"
+
+def saveInDataBase():
+    return "Not Implemented"
+    
+def listAllShoppingWithOutDate():
+    return "Not Implemented"
+    
+def listAllShoppingBetweenDates():
+    return "Not Implemented"
+
+def getTotalExpensesgWithOutDate():
+    return "Not Implemented"
+    
+def getTotalExpensesBetweenDate():
+    return "Not Implemented"    
+    
+def createNewCategoryInDataBase():
+    return "Not Implemented"
+    
+    
+#================================================================
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}/"
-api(TOKEN=TOKEN, BASE_URL=BASE_URL)
+api(TOKEN=TOKEN, BASE_URL=BASE_URL,timeout=5)
