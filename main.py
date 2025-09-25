996d3bec5b371ed98a6e56caaf78ff41320068af
notebook Hp <carlosmaycon443@gmail.com>
2025-09-25 11:22:46 -0300

    add venv, and make more code for bot

--- main.py
+++ main.py
@@ -4,45 +4,61 @@ from dotenv import load_dotenv
 import os
 
 
-
-
- 
 def api(TOKEN, BASE_URL):
-    r = requests.get(BASE_URL+"getMe")
+    r = requests.get(BASE_URL + "getMe")
 
-    if(r.status_code != 200):
+    if r.status_code != 200:
         print("Bot offline encerrando processo")
         return
-    
+
     print("Bot online, OK")
     offset = None
-    while(True):
-        
-        # OffSet Definindo apartir de qual mensagem pegar sendo a primeira a mais importante
-        params = {"timeout": 10,"offset":offset}
-        r = requests.get(BASE_URL+"getUpdates",params=params)
+    while True:
+        polingUpdate = getUpdates(TOKEN,BASE_URL, offset=offset)
 
-        polingUpdate = bot(r,offset=offset)
-        
-        if(polingUpdate != None):
-            print(polingUpdate["message"]["text"])
+        if polingUpdate is not None:
+            
             offset = int(polingUpdate["update_id"]) + 1
-        
-        
-def bot(r,offset = None):
-    print(r.json())
 
-    if(("result" not in r.json())):
+            recivedMessage(BASE_URL,polingUpdate["message"])
+        
+
+
+#message sera um objeto complexo contendo todos os dados da mensagem
+def recivedMessage(BASE_URL,message = None):  # noqa: F811
+    if message is None:
         return None
     
-    if(len(r.json()["result"]) <= 0):
+    chat_id = message["chat"]["id"]
+    user_first_name = message["chat"]["first_name"]
+
+
+    params = {"chat_id": chat_id,"text":f"hello {user_first_name}"}
+
+    requests.post(BASE_URL+"sendMessage",params=params)
+    
+
+
+def getUpdates(TOKEN, BASE_URL, offset=None):
+    # OffSet Definindo apartir de qual mensagem pegar sendo a primeira a mais importante
+
+    params = {"timeout": 5, "offset": offset}
+    r = requests.get(BASE_URL + "getUpdates", params=params)
+
+    print(r.json())
+
+    if "result" not in r.json():
         return None
-   # Para pegar as proximas mensagens
+
+    if len(r.json()["result"]) <= 0:
+        return None
+    
+    # Para pegar as proximas mensagens
     return r.json()["result"][0]
- 
- 
+
+
 load_dotenv()
- 
-TOKEN =  os.getenv("BOT_TOKEN")
+
+TOKEN = os.getenv("BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{TOKEN}/"
-api(TOKEN=TOKEN,BASE_URL=BASE_URL)
+api(TOKEN=TOKEN, BASE_URL=BASE_URL)
